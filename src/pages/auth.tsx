fad26df646bb79cc2a6824d2c8603bcf0b98a671
Baris Balli <barisbll.dev@gmail.com>
2023-07-24 12:04:16 +0300

    authentication page with the card component added

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,26 +1,31 @@
-import { signIn, signOut, useSession } from "next-auth/react";
-import { api } from "~/utils/api";
+import { signIn } from "next-auth/react";
 import { Button } from "~/components/ui/button";
-import { useRouter } from "next/router";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "~/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
+import { Github } from "lucide-react";
 
-const AuthShowcase: React.FC = () => {
-  const { data: sessionData, status } = useSession();
-  const router = useRouter();
-
-  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined }
+const Auth: React.FC = () => {
+  return (
+    <div className="flex w-full items-center justify-center pt-40">
+      <Card className="w-5/6 md:w-1/2 lg:w-1/3">
+        <CardHeader>
+          <CardTitle className="text-center text-xl font-light">
+            Create an Account or Login
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Button
+            variant="outline"
+            className="w-full"
+            // eslint-disable-next-line @typescript-eslint/no-misused-promises
+            onClick={() => signIn("github", { callbackUrl: "/" })}
+          >
+            <Github className="mr-2 h-4 w-4" />
+            <span>Github</span>
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
   );
-
-  return <div className="flex items-center justify-center"></div>;
 };
 
-export default AuthShowcase;
+export default Auth;
